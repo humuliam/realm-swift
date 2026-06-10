c05a3a4d55762c11f8a5783c99363e947d00abad
Hamed Nikbakht <hamed@nikbakht.me>
2026-06-10 13:03:45 +0200

    Update realm core package url

--- Package.swift
+++ Package.swift
@@ -59,7 +59,7 @@ let package = Package(
             targets: ["RealmSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/realm-core.git", exact: coreVersion)
+        .package(url: "https://github.com/humuliam/realm-core.git", branch: "master")
     ],
     targets: [
       .target(
